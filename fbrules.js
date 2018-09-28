7ebdc93e15667f7a423461295bedbab084c6ad10
Daniel Sim <daniel.ssq89@gmail.com>
2018-09-28 23:06:40 +0800

    add fb rules for reference

---
+++ fbrules.js
@@ -0,0 +1,81 @@
+{
+  "rules": {
+    "users": {
+      ".read": "true",
+      ".indexOn": ["email"],
+    },
+    "userDefaults": {
+      "$uid": {
+        ".read": "auth.uid == $uid",
+        ".write": "auth.uid == $uid",
+      }
+    },
+    "drivers": {
+      ".read": "true",
+    },
+    "admins": {
+      ".read": "auth.token.email_verified == true && (
+                  root.child('admins')
+                  .child(auth.token.email.replace('.', '%2e'))
+                  .val() == 1)",
+    },
+    "bookings": {
+      ".read": "auth.token.email_verified == true && (
+            root.child('admins')
+            .child(auth.token.email.replace('.', '%2e'))
+            .val() == 1)",
+      ".indexOn": ["pickupTime", "createdAt", "contactEmail"],
+      "$key": {
+        ".read": "
+            auth.token.email_verified == true && (
+            root.child('admins')
+            .child(auth.token.email.replace('.', '%2e'))
+            .val() == 1 ||
+            auth.token.email == data.child('contactEmail').val())",
+        ".write": "
+            auth.token.email_verified == true && (
+            root.child('admins')
+            .child(auth.token.email.replace('.', '%2e'))
+            .val() == 1 ||
+            (
+              auth.token.email == newData.child('contactEmail').val() &&
+              (
+                auth.token.email == data.child('contactEmail').val() ||
+                null == data.child('contactEmail').val()
+              )
+            ))
+        ",
+      }
+    },
+    "cancellations": {
+      ".read": "auth.token.email_verified == true && (
+            root.child('admins')
+            .child(auth.token.email.replace('.', '%2e'))
+            .val() == 1)",
+      ".indexOn": ["pickupTime", "createdAt", "contactEmail"],
+      "$key": {
+        ".read": "
+            auth.token.email_verified == true && (
+            root.child('admins')
+            .child(auth.token.email.replace('.', '%2e'))
+            .val() == 1 ||
+            auth.token.email == root.child('bookings').child($key).child('contactEmail').val())",
+        ".write": "
+            auth.token.email_verified == true && (
+            root.child('admins')
+            .child(auth.token.email.replace('.', '%2e'))
+            .val() == 1 ||
+            (auth.token.email == root.child('bookings').child($key).child('contactEmail').val())
+        ",
+        ".indexOn": ["createdAt"]
+      }
+    },
+    "userBookings":  {
+      "$uid": {
+        ".read": "auth.uid == $uid",
+        ".write": "auth.uid == $uid",
+        ".indexOn": ["createdAt", "pickupTime"]
+      }
+    },
+  },
+}
