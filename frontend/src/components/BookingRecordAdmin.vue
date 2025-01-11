c5b553c6e018614da15b6b78b4474c7e8e31b267
dssq <danielssq89@gmail.com>
2025-01-11 15:25:26 +0800

    crlf -> lf

--- frontend/src/components/BookingRecordAdmin.vue
+++ frontend/src/components/BookingRecordAdmin.vue
@@ -1,13 +1,23 @@
 <template>
-  <booking-record :booking="booking" :key="booking.id" :class="{
-    unread: !booking.read,
-    untrusted: !isTrusted(booking.contactEmail)
-  }" :now="now">
+  <booking-record
+    :booking="booking"
+    :key="booking.id"
+    :class="{
+      unread: !booking.read,
+      untrusted: !isTrusted(booking.contactEmail),
+    }"
+    :now="now"
+  >
     <v-toolbar flat>
       <v-btn icon @click="read" title="Mark as read">
         <v-icon>markunread_mailbox</v-icon>
       </v-btn>
-      <v-btn icon @click="cancel" title="Cancel" v-if="!booking.cancelled && !booking.cancelledByUser">
+      <v-btn
+        icon
+        @click="cancel"
+        title="Cancel"
+        v-if="!booking.cancelled && !booking.cancelledByUser"
+      >
         <v-icon>delete</v-icon>
       </v-btn>
       <v-btn icon @click="reopen" title="Uncancel" v-else>
@@ -17,58 +27,51 @@
   </booking-record>
 </template>
 <script>
-
-import { mapState, mapActions } from 'vuex';
-const { fbDB } = require('../firebase');
-import BookingRecord from './BookingRecord.vue'
-import { ref, update } from 'firebase/database'
+import { mapState, mapActions } from "vuex";
+const { fbDB } = require("../firebase");
+import BookingRecord from "./BookingRecord.vue";
+import { ref, update } from "firebase/database";
 
 export default {
-  props: ['booking'],
+  props: ["booking"],
   computed: {
-    ...mapState(['trustedEmails', 'isTrusted', 'now']),
+    ...mapState(["trustedEmails", "isTrusted", "now"]),
   },
   components: {
-    BookingRecord
+    BookingRecord,
   },
   methods: {
-    ...mapActions(['loadingSpinner', 'flashError']),
+    ...mapActions(["loadingSpinner", "flashError"]),
     read() {
-      update(ref(fbDB(), `/bookings/${this.booking.id}`),
-        {
-          read: !this.booking.read
-        })
-        .catch((err) => {
-          this.flashError({
-            ...err,
-            type: 'error'
-          })
-        })
+      update(ref(fbDB(), `/bookings/${this.booking.id}`), {
+        read: !this.booking.read,
+      }).catch((err) => {
+        this.flashError({
+          ...err,
+          type: "error",
+        });
+      });
     },
     cancel() {
-      update(ref(fbDB(), `/bookings/${this.booking.id}`),
-        {
-          cancelled: true
-        })
-        .catch((err) => {
-          this.flashError({
-            ...err,
-            type: 'error'
-          })
-        })
+      update(ref(fbDB(), `/bookings/${this.booking.id}`), {
+        cancelled: true,
+      }).catch((err) => {
+        this.flashError({
+          ...err,
+          type: "error",
+        });
+      });
     },
     reopen() {
-      update(ref(fbDB(), `/bookings/${this.booking.id}`),
-        {
-          cancelled: false
-        })
-        .catch((err) => {
-          this.flashError({
-            ...err,
-            type: 'error'
-          })
-        })
-    }
-  }
-}
+      update(ref(fbDB(), `/bookings/${this.booking.id}`), {
+        cancelled: false,
+      }).catch((err) => {
+        this.flashError({
+          ...err,
+          type: "error",
+        });
+      });
+    },
+  },
+};
 </script>
